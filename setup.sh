bd3ff342a8fab5653457652326d926d0b97554dd
Anastasios Angelopoulos <anastasiosna@gmail.com>
2020-08-13 00:18:42 -0700

    [setup] finished setup

--- setup.sh
+++ setup.sh
@@ -12,4 +12,104 @@ wget -v -O user9.tar.gz -L https://berkeley.box.com/shared/static/i34p1v1k0oq8it
 tar -xzf user9.tar.gz
 rm user9.tar.gz
 
+#wget -v -O user8.tar.gz -L https://berkeley.box.com/shared/static/eu8l5wof12j8nwvijogc11nh7e6yb0jn 
+#tar -xzf user8.tar.gz
+#rm user8.tar.gz
+#
+#wget -v -O user7.tar.gz -L https://berkeley.box.com/shared/static/eu8l5wof12j8nwvijogc11nh7e6yb0jn 
+#tar -xzf user7.tar.gz
+#rm user7.tar.gz
+#
+#wget -v -O user6.tar.gz -L https://berkeley.box.com/shared/static/lc81wo13l88l1gqa2e3hrzmkliy9joug 
+#tar -xzf user6.tar.gz
+#rm user6.tar.gz
+#
+#wget -v -O user5.tar.gz -L https://berkeley.box.com/shared/static/9h7s2lapvuoflbclunaadzo76cfgabg7 
+#tar -xzf user5.tar.gz
+#rm user5.tar.gz
+#
+#wget -v -O user4.tar.gz -L https://berkeley.box.com/shared/static/c45qzm9q8es19cesuolgu0y1nvl8ecwd 
+#tar -xzf user4.tar.gz
+#rm user4.tar.gz
+#
+#wget -v -O user2.tar.gz -L https://berkeley.box.com/shared/static/4p9urgsfuawrvmjrgjwqkclm2y9qd6ta 
+#tar -xzf user2.tar.gz
+#rm user2.tar.gz
+#
+#wget -v -O user27.tar.gz -L https://berkeley.box.com/shared/static/elby39i52hilo8oabis3m9axixytvvu8 
+#tar -xzf user27.tar.gz
+#rm user27.tar.gz
+#
+#wget -v -O user26.tar.gz -L https://berkeley.box.com/shared/static/m8kloftvnx5agrlq0af1e3oc6a6cuam0 
+#tar -xzf user26.tar.gz
+#rm user26.tar.gz
+#
+#wget -v -O user25.tar.gz -L https://berkeley.box.com/shared/static/q2zg1jsqpuum06l0nv8g72qnkjf4nkvt 
+#tar -xzf user25.tar.gz
+#rm user25.tar.gz
+#
+#wget -v -O user24.tar.gz -L https://berkeley.box.com/shared/static/6q7jddf4y9lt7o90dtvxr1uuep35qz1k 
+#tar -xzf user24.tar.gz
+#rm user24.tar.gz
+#
+#wget -v -O user23.tar.gz -L https://berkeley.box.com/shared/static/8xzsrk9tvdox1dy0j13izrclebxh1yot 
+#tar -xzf user23.tar.gz
+#rm user23.tar.gz
+#
+#wget -v -O user22.tar.gz -L https://berkeley.box.com/shared/static/8u0v2p6lyirblnuxqrn8x2m174uw37wn 
+#tar -xzf user22.tar.gz
+#rm user22.tar.gz
+#
+#wget -v -O user21.tar.gz -L https://berkeley.box.com/shared/static/durpw3ek1w49q1bmev7x1h22qbh0086p 
+#tar -xzf user21.tar.gz
+#rm user21.tar.gz
+#
+#wget -v -O user20.tar.gz -L https://berkeley.box.com/shared/static/vd1robka5i0drq26y52nlmuwxn9b2khz 
+#tar -xzf user20.tar.gz
+#rm user20.tar.gz
+#
+#wget -v -O user1.tar.gz -L https://berkeley.box.com/shared/static/kiqwtws48fl3p5p9uxck0btrnt9s49u9 
+#tar -xzf user1.tar.gz
+#rm user1.tar.gz
+#
+#wget -v -O user19.tar.gz -L https://berkeley.box.com/shared/static/npag771b6q05f3n7rp86d5ifclm9fob6 
+#tar -xzf user19.tar.gz
+#rm user19.tar.gz
+#
+#wget -v -O user18.tar.gz -L https://berkeley.box.com/shared/static/lv24h2547osrb3nxf32rtjsvacyz3mac 
+#tar -xzf user18.tar.gz
+#rm user18.tar.gz
+#
+#wget -v -O user17.tar.gz -L https://berkeley.box.com/shared/static/73hb3eq3d0xbfm3deqbu79o83c23wdr3 
+#tar -xzf user17.tar.gz
+#rm user17.tar.gz
+#
+#wget -v -O user16.tar.gz -L https://berkeley.box.com/shared/static/4htakz1sdik5byevg13avzcg2jd8aeao 
+#tar -xzf user16.tar.gz
+#rm user16.tar.gz
+#
+#wget -v -O user15.tar.gz -L https://berkeley.box.com/shared/static/df7ucf35yohms4rhensaluco4mu1qvv8 
+#tar -xzf user15.tar.gz
+#rm user15.tar.gz
+#
+#wget -v -O user14.tar.gz -L https://berkeley.box.com/shared/static/igh0ie6undxo7ukpyqgwwh1s4rxjjd89 
+#tar -xzf user14.tar.gz
+#rm user14.tar.gz
+#
+#wget -v -O user13.tar.gz -L https://berkeley.box.com/shared/static/2w02ce31xua66dwkf4b02x35nrk7h8h3 
+#tar -xzf user13.tar.gz
+#rm user13.tar.gz
+#
+#wget -v -O user12.tar.gz -L https://berkeley.box.com/shared/static/b286t6us26m9dr70wqrpivsj0o4nifgy 
+#tar -xzf user12.tar.gz
+#rm user12.tar.gz
+#
+#wget -v -O user11.tar.gz -L https://berkeley.box.com/shared/static/sqxmtafmusr0me6sdd8qfx2wccd9wmb3 
+#tar -xzf user11.tar.gz
+#rm user11.tar.gz
+#
+#wget -v -O user10.tar.gz -L https://berkeley.box.com/shared/static/0epk0j6seu27oc0ixplm6v82ixv231u3 
+#tar -xzf user10.tar.gz
+#rm user10.tar.gz
+
 cd ..
